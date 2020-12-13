fa8bcf37db9a4074fa3ce7dbc4c92fec703f51d4
nArrow4 <3284799532@qq.com>
2020-12-13 23:18:39 +0800

    C Solution

--- 139.单词拆分.c
+++ 139.单词拆分.c
@@ -5,10 +5,38 @@
  */
 
 // @lc code=start
+#include <string.h>
 
+int check(char *s, char ** wordDcit, int wordDictSize, int i, int j){
+    char *tmp = (char *)malloc(sizeof(char) * (strlen(s) + 1));
+    tmp = s + j + 1;
+    tmp[i - j] = '\0';      //j+1 to i
+    // for (int k = 0; k < wordDictSize; k++){
+    //     if(strcmp(tmp, wordDcit[k]) == 1){
+    //         free(tmp);
+    //         return true;
+    //     }
+    // }
+    return false;
+}
 
 bool wordBreak(char * s, char ** wordDict, int wordDictSize){
-    
+    int len = strlen(s);
+    bool dp[len + 1];
+    memset(dp, 0, sizeof(bool) * (len + 1));
+    dp[0] = true;
+    for (int i = 1; i <= len; i++){     //for 1 to len
+        for (int j = 0; j < i; j++){    //for 0 to i-1
+            printf("%d ", dp[j]);
+            //s:j+1 to i -> 1 to 
+            if(check(s, wordDict, wordDictSize, i, j) && dp[j]){  
+                dp[i] = true; 
+                break;
+            }
+        }
+        printf("%d ", dp[i]);
+    }
+    return dp[len];
 }
 // @lc code=end
 
---
+++ 140.单词拆分-ii.c
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=140 lang=c
+ *
+ * [140] 单词拆分 II
+ */
+
+// @lc code=start
+
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+char ** wordBreak(char * s, char ** wordDict, int wordDictSize, int* returnSize){
+
+}
+// @lc code=end
+
