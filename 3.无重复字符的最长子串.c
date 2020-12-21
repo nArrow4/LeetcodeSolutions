f39d6d8efb5049c575ff5095b72941e707f9cba0
nArrow4 <3284799532@qq.com>
2020-12-21 20:07:33 +0800

    C solution

---
+++ 3.无重复字符的最长子串.c
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=3 lang=c
+ *
+ * [3] 无重复字符的最长子串
+ */
+
+// @lc code=start
+
+#include<string.h>
+int lengthOfLongestSubstring(char * s){
+    // int count[26], len = strlen(s), i, pos = -1;
+    // int left, right, ans = 0;
+    // char res[len];
+    // left = right = 0;
+    // for (int i = 0; i < len;i++){
+    //     if(s[i]>'z' || s[i]<'a')
+    //         continue;
+    //     else
+    //         res[++pos] = s[i];
+    // }
+    // while(left <= right && right < len){
+    //     while(right < len && count[res[right]] == 0){
+    //         right++;
+    //         ans = fmax(ans, right - left + 1);
+    //     }
+
+    // }
+    // return ans;
+}
+// @lc code=end
+
