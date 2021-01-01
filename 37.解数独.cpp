accf0ecfa77f967dd79f7ab87c17ac23c9e3b89b
nArrow4 <3284799532@qq.com>
2021-01-01 10:43:11 +0800

    C solution

---
+++ 37.解数独.cpp
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=37 lang=cpp
+ *
+ * [37] 解数独
+ */
+
+// @lc code=start
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    void solveSudoku(vector<vector<char>>& board) {
+        
+    }
+};
+// @lc code=end
+
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# LeetCode题解——C语言版
+# LeetCode题解——C & cpp版
 
 记录刷过的题的题解
+
+--update:2021/1/1开始用cpp刷题啦
